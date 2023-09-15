25e74d4c49ce6f5c82af58219c6f92092b1efdac
MMJLee <matthewmjlee@gmail.com>
2023-09-14 20:19:45 -0500

    use dockerfile for env var

--- server/index.js
+++ server/index.js
@@ -7,15 +7,13 @@ import cors from 'cors';
 const port = 3000;
 const app = express();
 app.use(cors({
-  // origin: ["http://127.0.0.1:5173"],
-  origin:["https://chat.mjlee.dev"], 
+  origin: process.env.CLIENT_URL || "http://127.0.0.1:5173",
 }));
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   path: "/chat/socketio/",
   cors: {
-    // origin: ["http://127.0.0.1:5173"],
-    origin:["https://chat.mjlee.dev"], 
+    origin: process.env.CLIENT_URL || "http://127.0.0.1:5173",
     credentials: true,
     methods: ["GET", "POST"],
   }
